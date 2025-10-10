ab131e234d11fc421ad0c30c699db020c1d74bb6
NathanielWatife <awosanminathaniel0@gmail.com>
2025-10-10 21:49:58 +0100

    changes

--- users/models.py
+++ users/models.py
@@ -1,10 +1,35 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 import uuid
 from datetime import timedelta
 from django.utils import timezone
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The mail field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser with an email and password"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
+        extra_fields.setdefault('role', 'super_admin')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
     ROLE_CHOICES = [
         ('student', 'Student'),
@@ -30,13 +55,15 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'matric_no', 'department', 'level']
 
+    objects = CustomUserManager()
+
     def __str__(self):
         return f"{self.matric_no} - {self.email}"
     
     def generate_verification_code(self):
         import random
         self.verification_code = str(random.randint(100000, 999999))
-        self.verification_code_expires = timezone.now() + timedelta(minutes=24)
+        self.verification_code_expires = timezone.now() + timedelta(minutes=30)
         self.save()
         return self.verification_code
     
