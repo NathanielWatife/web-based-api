5962ef1fcbc84a88620b03d695a0cc9d25b73cfc
NathanielWatife <awosanminathaniel0@gmail.com>
2025-10-20 11:54:46 +0100

    made changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,14 +5,14 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
     },
   });
 
   // Define email options
   const mailOptions = {
-    from: `YabaTech BookStore <${process.env.EMAIL_USERNAME}>`,
+    from: `YabaTech BookStore <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     html: options.html,
